91169128560092af2ad543f88e1d6f433bc8edc4
Giovanna Andrade <130957052+giovannaba@users.noreply.github.com>
2023-10-24 16:43:58 -0300

    atualizando

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function newTask (){
+    let input = document.getElementById('input-new-task')
+    
+    if(!input.value){
+        alert('Digite algo para inserir na sua lista')
+    // } else if () {
 
+    // }
+    else {
+        //incremente to localStorage
+        let values = JSON.parse()
+    }
+}
